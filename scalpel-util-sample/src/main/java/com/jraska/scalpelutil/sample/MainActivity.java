58f39a7f9df2a5dd388c9133bb4f83478439f848
Josef Raska <josef.raska@gmail.com>
2015-12-10 21:06:50 +0100

    Add build variables

--- scalpel-util-sample/src/main/java/com/jraska/scalpelutil/sample/MainActivity.java
+++ scalpel-util-sample/src/main/java/com/jraska/scalpelutil/sample/MainActivity.java
@@ -1,4 +1,4 @@
-package com.jraska.scalpelutil;
+package com.jraska.scalpelutil.sample;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.jraska.scalpelutil.R;
 
 public class MainActivity extends AppCompatActivity {
 
